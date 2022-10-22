4f5172de34a132e093ec340dbaa1622ab81ac08d
reflexsafwan <reflex.safwan@gmail.com>
2022-10-22 21:21:04 +0600

    router created corectly

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <div>This is heading</div>
+    },
+  ])
   return (
     <div className="App">
-
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
