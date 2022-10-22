c83a49f8d72826507f1f6740cb44702e6989eca9
reflexsafwan <reflex.safwan@gmail.com>
2022-10-22 21:28:56 +0600

    Header component adde with menu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   const router = createBrowserRouter([
     {
       path: '/',
-      element: <div>This is heading</div>
+      element: <Header></Header>
     },
   ])
   return (
